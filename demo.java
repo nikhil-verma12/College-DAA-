07de1119ff4cf616c229eac8322615c87a35832e
Nikhil Verma <nikhilverma22012007@gmail.com>
2025-09-30 22:07:43 +0530

    temp

--- demo.java
+++ demo.java
@@ -1,7 +1,94 @@
 public class demo{
     public static void main(String args[]){
-        System.out.println("Hello World");
-        int a = 10;
+        System.out.println("Hello World. My First Program...");
         System.out.println(a);
     }
 }
+
+Skip to content
+Navigation Menu
+nikhil-verma12
+College-DAA-
+
+Type / to search
+Code
+Issues
+Pull requests
+Actions
+Projects
+Wiki
+Security
+Insights
+Settings
+Owner avatar
+College-DAA-
+Public
+nikhil-verma12/College-DAA-
+Go to file
+t
+Name		
+nikhil-verma12
+nikhil-verma12
+new program
+aada6f4
+ · 
+8 minutes ago
+README.md
+Initial commit
+46 minutes ago
+demo.class
+new program
+8 minutes ago
+demo.java
+new program
+8 minutes ago
+Repository files navigation
+README
+College-DAA-
+Lab File Program
+
+About
+Lab File Program
+
+Resources
+ Readme
+ Activity
+Stars
+ 0 stars
+Watchers
+ 0 watching
+Forks
+ 0 forks
+Releases
+No releases published
+Create a new release
+Packages
+No packages published
+Publish your first package
+Languages
+Java
+100.0%
+Suggested workflows
+Based on your tech stack
+Publish Java Package with Maven logo
+Publish Java Package with Maven
+Build a Java Package using Maven and publish to GitHub Packages.
+Android CI logo
+Android CI
+Build an Android project with Gradle.
+Java with Gradle logo
+Java with Gradle
+Build and test a Java project using a Gradle wrapper script.
+More workflows
+Footer
+© 2025 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+Security
+Status
+Community
+Docs
+Contact
+Manage cookies
+Do not share my personal information
